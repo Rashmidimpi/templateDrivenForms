bc58c39ae9c0c03e2dd8db7fd7d863c9620ef67f
rahul-dudhane-shortlist <rahul.dudhane@shortlist.net>
2023-02-19 22:59:58 +0530

    "latest commit +1"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'forms';
+  defaultJob= 'fullstack_developer';
+  firstName:string | undefined;
+  
+  @ViewChild('myForm') form: NgForm | undefined;
+
+  onSubmit(){
+    console.log(this.form);
+  }
 }
